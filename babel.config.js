5655acbc2309bad384765ff0cf308ea86f6209a1
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-02-18 05:01:23 +0530

    Added react-navigation drawer support

--- babel.config.js
+++ babel.config.js
@@ -10,7 +10,8 @@ module.exports = function(api) {
         "safe": false,
         "allowUndefined": true,
         "verbose": false
-      }]
+      }],
+      'react-native-reanimated/plugin'
     ]
   };
 };
