a84617e203e94b43f10da31e0c9dba7682fc883c
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-02-22 04:54:00 +0530

    Added missing icons

--- src/Shared/SVG.ts
+++ src/Shared/SVG.ts
@@ -1,9 +1,15 @@
 import DiscordLight from "../../assets/discordLight.svg"
 import DiscordDark from "../../assets/discordDark.svg"
 import HamburgerMenu from "../../assets/hamburger-menu.svg"
+import ArrowDown from "../../assets/arrowDown.svg"
+import ArrowLeft from "../../assets/arrowLeft.svg"
+import CheckMark from "../../assets/checkMark.svg"
 
 export {
     DiscordDark,
     DiscordLight,
-    HamburgerMenu
+    HamburgerMenu,
+    ArrowDown,
+    ArrowLeft,
+    CheckMark
 }
