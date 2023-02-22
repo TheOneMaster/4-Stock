88003eac30bdf3de2b6b4af9a165c870801e1548
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-02-22 05:43:08 +0530

    Added SettingsSwitch component

---
+++ src/Settings/SettingsSwitch.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Switch, Text, View } from "react-native"
+import { SettingsProps } from "./types";
+import { useState } from "react";
+import SettingsItem from "./SettingsItem";
+import { useTheme } from "@react-navigation/native";
+import { SettingsItemStyles } from "./types";
+
+
+const SettingsSwitch = ({title, setting, style}: SettingsProps) => {
+    
+    const [active, setActive] = useState(false);
+    const {colors} = useTheme();
+
+
+    function toggleSwitch(state) {
+        setActive(state);
+        console.log(state);
+    }
+
+    return (
+    <View style={[SettingsItemStyles.container, {borderColor: colors.border}, style]}>
+        <Text style={[SettingsItemStyles.title, {color: colors.text}]}>{title}</Text>
+        <View style={SettingsItemStyles.componentContainer}>
+            <Switch value={active} onValueChange={toggleSwitch}/>
+        </View>
+
+    </View>
+    )
+}
+
+export default SettingsSwitch;
--- src/Settings/types.ts
+++ src/Settings/types.ts
@@ -1,18 +1,38 @@
-import { StyleProp, ViewStyle } from "react-native"
-
-export interface SettingsProps {
-    title: string,
-    setting?: string
-}
-
-export interface SettingsDropdownProps extends SettingsProps {
-    data: DropdownOption[],
-    value?: number,
-    style?: StyleProp<ViewStyle> | StyleProp<ViewStyle>[]
-}
+import { StyleProp, StyleSheet, ViewStyle } from "react-native"
 
 export interface DropdownOption {
     id?: string,
     label: string,
     value: number
 }
+
+export interface SettingsProps {
+    title: string,
+    setting: string,
+    style?: StyleProp<ViewStyle> | StyleProp<ViewStyle>[]
+}
+
+export interface SettingsDropdownProps extends SettingsProps {
+    data: DropdownOption[],
+    value?: number,
+    backgroundColor?: string,
+}
+
+export const SettingsItemStyles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 10,
+        minHeight: 50,
+
+        borderBottomWidth: 1,
+        borderStyle: 'solid'
+    },
+    title: {
+        fontSize: 15,
+        fontWeight: 'bold'
+    },
+    componentContainer: {
+        marginLeft: 'auto'
+    }
+})
