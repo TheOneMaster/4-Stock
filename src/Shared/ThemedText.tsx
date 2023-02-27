15493d4e8f682c312f10e25df841d414d9749dff
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-02-27 16:48:05 +0530

    Theme text components now accept Text props

--- src/Shared/ThemedText.tsx
+++ src/Shared/ThemedText.tsx
@@ -1,24 +1,20 @@
-import { Text, TextProps } from "react-native";
 import { useTheme } from "@react-navigation/native";
+import { Text, TextProps } from "react-native";
 
 export function MainText(props: TextProps) {
     const { colors } = useTheme();
     const propStyle = props.style;
-    const children = props.children;
 
     delete props.style;
-    delete props.children;
 
-    return <Text style={[{color: colors.text}, propStyle]} {...props}>{children}</Text>
+    return <Text style={[{ color: colors.text }, propStyle]} {...props}>{props.children}</Text>
 }
 
 export function SubtitleText(props: TextProps) {
-    const {colors} = useTheme();
+    const { colors } = useTheme();
     const propStyle = props.style;
-    const children = props.children;
 
     delete props.style;
-    delete props.children;
 
-    return <Text style={[{color: colors.secondaryText}, propStyle]} {...props}>{children}</Text>
+    return <Text style={[{ color: colors.secondaryText }, propStyle]} {...props}>{props.children}</Text>
 }
