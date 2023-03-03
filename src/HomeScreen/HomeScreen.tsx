d968ed19c88230f870847ae6d7f92e3a1e9b7403
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-03-03 16:46:33 +0530

    Formatting

--- src/HomeScreen/HomeScreen.tsx
+++ src/HomeScreen/HomeScreen.tsx
@@ -1,40 +1,39 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useTheme } from "@react-navigation/native";
-import { StyleSheet, Text, View } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { StyleSheet } from "react-native";
 
 import { HomeDrawerParamList, RootStackParamList } from "../navTypes";
 
-import TournamentListView from "./TournamentListView";
-import SettingsPage from "../Settings/SettingsPage";
 import AboutPage from "../About/AboutPage";
+import SettingsPage from "../Settings/SettingsPage";
 import CustomDrawerContent from "./CustomDrawerContent";
+import TournamentListView from "./TournamentListView";
 
 const Drawer = createDrawerNavigator<HomeDrawerParamList>();
 
-const HomeScreen = ({navigation, route}: NativeStackScreenProps<RootStackParamList, "Home">) => {
+const HomeScreen = ({ navigation, route }: NativeStackScreenProps<RootStackParamList, "Home">) => {
 
     const { colors } = useTheme();
 
     return (
         <Drawer.Navigator screenOptions={{
             swipeEdgeWidth: 200,
-            headerStyle: {backgroundColor: colors.primary},
-            headerTitleStyle: {color: colors.text}
-            }}
-            
+            headerStyle: { backgroundColor: colors.primary },
+            headerTitleStyle: { color: colors.text }
+        }}
             drawerContent={(props) => <CustomDrawerContent {...props} />}
-            
-            >
-            <Drawer.Screen name="Tournaments" component={TournamentListView}/>
-            <Drawer.Screen name="Settings" component={SettingsPage}/>
+
+        >
+            <Drawer.Screen name="Tournaments" component={TournamentListView} />
+            <Drawer.Screen name="Settings" component={SettingsPage} />
             <Drawer.Screen name="About" component={AboutPage} />
         </Drawer.Navigator>
     )
 }
 
 const styles = StyleSheet.create({
-    
+
 })
 
 export default HomeScreen;
