8097facdd4ee88e01bc77f6fde14a0a141019637
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-03-20 19:00:57 +0530

    Added carousels for tournaments & leagues

--- src/Profile/ProfilePage.tsx
+++ src/Profile/ProfilePage.tsx
@@ -1,14 +1,19 @@
+import { useTheme } from "@react-navigation/native";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+
 import { queryAPI, userDetailsQuery } from "../api";
 import { UserProfileProps } from "../navTypes";
+import { convertLeagueToCarouselItem, convertTournamentToCarouselItem, convertUserEventToCarouselItem } from "../Shared/APIConverters";
+import DetailsCarousel from "../Shared/DetailsCarousel/DetailsCarousel";
+import { MainText } from "../Shared/ThemedText";
 import { User, UserDetails } from "../types";
-import EventDetailsCarousel from "./DetailsCarousel";
 import ProfileHeader from "./ProfileHeader";
 
 function UserProfilePage({ navigation, route }: UserProfileProps) {
     const [loading, setLoading] = useState(true);
     const [profileDetails, setProfileDetails] = useState<User>(null);
+    const { colors } = useTheme();
 
     useEffect(() => {
         // Get user details from ID
@@ -23,24 +28,38 @@ function UserProfilePage({ navigation, route }: UserProfileProps) {
     }, [])
 
     if (loading) return (
-        <View style={styles.container}>
-            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                <Text>Loading...</Text>
-            </View>
-        </View>)
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <MainText>Loading...</MainText>
+        </View>
+    )
 
     return (
         <View style={styles.container}>
             <ProfileHeader profileDetails={profileDetails} />
 
             <View style={styles.infoSection}>
-                <EventDetailsCarousel profileDetails={profileDetails.events.nodes} title={"Past Events"} />
+                <DetailsCarousel
+                    style={{ marginTop: 10 }}
+                    data={convertUserEventToCarouselItem(profileDetails.events.nodes, colors.primary)}
+                    title="Previous Events"
+                    emptyText="No events found"
+                />
+
+                <DetailsCarousel
+                    style={styles.carousel}
+                    data={convertTournamentToCarouselItem(profileDetails.tournaments.nodes)}
+                    title="Tournaments"
+                    emptyText="No tournaments found"
+                />
+
+                <DetailsCarousel
+                    style={styles.carousel}
+                    data={convertLeagueToCarouselItem(profileDetails.leagues.nodes)}
+                    title="Leagues"
+                    emptyText="No leagues found"
+                />
+
             </View>
-
-
-
-
-
 
         </View>
     )
@@ -48,10 +67,14 @@ function UserProfilePage({ navigation, route }: UserProfileProps) {
 
 const styles = StyleSheet.create({
     container: {
+        height: "100%"
     },
     infoSection: {
         paddingHorizontal: 10,
-        marginTop: 10
+        flexGrow: 1,
+    },
+    carousel: {
+        marginTop: 30
     }
 })
 
