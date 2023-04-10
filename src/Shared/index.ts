c0e8d0ce8b3edf12350a4d95dda48d85947d2f5c
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2023-04-10 10:19:14 +0530

    formatting

--- src/Shared/index.ts
+++ src/Shared/index.ts
@@ -1,15 +1,12 @@
-// import DetailsCarousel from "./DetailsCarousel/DetailsCarousel";
-// import { CarouselDataItem } from "./DetailsCarousel/types";
-// import EyeTextInput from "./EyeTextInput/EyeTextInput";
 export * from "./Containers/Containers";
 export * from "./APIConverters"
 export * from "./ThemedText"
 
-export {default as DetailsCarousel} from "./DetailsCarousel/DetailsCarousel"
-export {default as EyeTextInput} from "./EyeTextInput/EyeTextInput"
-export {default as SearchBar} from "./SearchBar"
-export {default as PlaceholderImage} from "./PlaceholderImage"
+export { default as DetailsCarousel } from "./DetailsCarousel/DetailsCarousel"
+export { default as EyeTextInput } from "./EyeTextInput/EyeTextInput"
+export { default as SearchBar } from "./SearchBar"
+export { default as PlaceholderImage } from "./PlaceholderImage"
 
 import { CarouselDataItem } from "./DetailsCarousel/types";
 
-export {CarouselDataItem}
+export { CarouselDataItem }
