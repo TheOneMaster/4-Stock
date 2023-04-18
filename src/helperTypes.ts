1308fc5a1ac497f0b28acf243409c5a46d12cdb4
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2023-04-18 15:22:11 +0530

    Update navigation components
    - Debug group added to DrawerNavigation

--- src/helperTypes.ts
+++ src/helperTypes.ts
@@ -1,2 +1,6 @@
 export type NodeArray<T extends { nodes: unknown[] | null } | null> = Exclude<Exclude<T, null>['nodes'], null>;
 export type PropertyDetails<T, K extends keyof T> = Exclude<Exclude<T, null>[K], null>;
+
+export type NonNullProperties<T, K extends keyof T> = T & {
+    [P in K]: Exclude<T[P], null>
+}
