465e1f164a479712f1ef3833ae051330394fb539
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2023-04-21 12:55:48 +0530

    updated dev build script

--- scripts/devBuild.sh
+++ scripts/devBuild.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-mkdir -p "./build"
-EAS_LOCAL_BUILD_ARTIFACTS_DIR="./build"
+mkdir -p "./build/"
+# mkdir -p "./builddir/"
+
+EAS_LOCAL_BUILD_ARTIFACTS_DIR="$PWD/build/"
+# EAS_LOCAL_BUILD_WORKINGDIR="$PWD/builddir"
 
 eas build --platform android --profile development --local
