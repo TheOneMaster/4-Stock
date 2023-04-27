34ad723327381006c9c50f2ba21186cf54037ace
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2023-04-27 20:04:21 +0530

    remove unnecessary import from jest-setup

--- jest-setup.js
+++ jest-setup.js
@@ -1,4 +1,2 @@
-import '@testing-library/jest-native/extend-expect'
-
 global.__reanimatedWorkletInit = () => { };
 jest.mock("react-native-reanimated", () => require("react-native-reanimated/mock"));
