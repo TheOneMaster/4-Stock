6c384a76a1e4a84db0fad1d97062d70e3557fb1d
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-06-03 18:08:03 +0530

    Only add rounds to bracket if they contain sets
    - Moved gesture detection to bracket component instead of root page

--- src/MainScreen/Debug/Main.tsx
+++ src/MainScreen/Debug/Main.tsx
@@ -1,11 +1,9 @@
 import { StyleSheet, View } from "react-native";
 import { DebugPageProps } from "../../navTypes";
-import { BracketTree } from "./BracketTree";
-import { Gesture, GestureDetector } from "react-native-gesture-handler";
-import { useDerivedValue, useSharedValue } from "react-native-reanimated";
 
-import SetData from "./Sets.json"
 import { SetQuery } from "../../Event/Bracket/types";
+import { GameBracket } from "./Bracket";
+import SetData from "./Sets.json";
 
 export type BracketRounds = {
     [round: number]: ReturnType<typeof reduceSets>
@@ -27,17 +25,20 @@ export function convertSets(sets: SetQuery) {
     const maxRound = Math.max(...setMap.map(set => set.round ?? -1));
     const minRound = Math.min(...setMap.map(set => set.round ?? 1));
 
-    console.log(maxRound);
     const winnersBracket: BracketRounds = {}
     for (let i=0;i<=maxRound;i++) {
         const setsInRound = setMap.filter(set => set.round === i);
-        winnersBracket[i] = setsInRound;
+        if (setsInRound.length > 0) {
+            winnersBracket[i] = setsInRound;
+        }
     }
 
     const losersBracket: BracketRounds = {};
     for (let i=-1;i>minRound;i--) {
         const setsInRound = setMap.filter(set => set.round === i);
-        losersBracket[i] = setsInRound;
+        if (setsInRound.length > 0) {
+            losersBracket[i] = setsInRound;
+        }
     }
 
     return {
@@ -49,41 +50,14 @@ export function convertSets(sets: SetQuery) {
 
 
 export function DebugPage(props: DebugPageProps) {
-
-    const translateX = useSharedValue(0);
-    const translateY = useSharedValue(0);
-    const context = useSharedValue({x: 0, y: 0});
-
-    const clampedTranslateX = useDerivedValue(() => {
-        return Math.min(translateX.value, 0);
-    });
-    const clampedTranslateY = useDerivedValue(() => {
-        return Math.min(translateY.value, 0);
-    })
-
-    const panGesture = Gesture.Pan().onStart(event => {
-        context.value = {y: clampedTranslateY.value, x: clampedTranslateX.value};
-    }).onUpdate(event => {
-        translateX.value = event.translationX + context.value.x;
-        translateY.value = event.translationY + context.value.y;
-
-        // console.log(translateX.value)
-
-    }).onEnd(event => {
-
-    });
-
     const sets = convertSets(SetData);
-    console.log(sets.winners)
-    
 
     return (
-        <GestureDetector gesture={panGesture}>
-            <View style={styles.mainPage}>
-                <BracketTree translateX={clampedTranslateX} translateY={clampedTranslateY} bracket={sets.winners} />
-            </View>
-        </GestureDetector>
-
+        <>
+            {/* <GameBracket title="Winners" bracket={sets.winners} /> */}
+            {/* <GameBracket title="Losers" bracket={sets.losers} /> */}
+            <GameBracket bracket={sets} />
+        </>
     )
 }
 
@@ -91,5 +65,10 @@ const styles = StyleSheet.create({
     mainPage: {
         flex: 1,
         // backgroundColor: "blue"
+    },
+    seperator: {
+        width: "100%",
+        height: 10,
+        backgroundColor: "grey"
     }
 })
