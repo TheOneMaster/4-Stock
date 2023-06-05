b24995d09fdcf20073db7074caa75028790d7910
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-06-05 21:05:39 +0530

    Move data manipulation functions to one file

--- src/MainScreen/Debug/BracketData.ts
+++ src/MainScreen/Debug/BracketData.ts
@@ -1,9 +1,44 @@
-import { BracketRounds } from "./Main";
-import { convertSets } from "./Main"
-import { BracketData, FullBracket, FullBracketData } from "./types";
+import { BracketData, BracketRounds, FullBracket, FullBracketData, SetList } from "./types";
 
 
+export function reduceSets(sets: SetList) {
+    return sets.map(set => {
+        return {
+            id: set?.id,
+            round: set?.round,
+            slots: set?.slots ?? []
+        }
+    }) ?? [];
+}
 
+export function convertSets(sets: SetList) {
+    const setMap = reduceSets(sets);
+
+    const maxRound = Math.max(...setMap.map(set => set.round ?? -1));
+    const minRound = Math.min(...setMap.map(set => set.round ?? 1));
+
+    const winnersBracket: BracketRounds = {}
+    for (let i=0;i<=maxRound;i++) {
+        const setsInRound = setMap.filter(set => set.round === i);
+        if (setsInRound.length > 0) {
+            winnersBracket[i] = setsInRound;
+        }
+    }
+
+    const losersBracket: BracketRounds = {};
+    for (let i=-1;i>minRound;i--) {
+        const setsInRound = setMap.filter(set => set.round === i);
+        if (setsInRound.length > 0) {
+            losersBracket[i] = setsInRound;
+        }
+    }
+
+    return {
+        winners: winnersBracket,
+        losers: losersBracket
+    }
+
+}
 
 function bracketAnalysis(bracket: BracketRounds): BracketData {
     const rounds = Object.keys(bracket).map((round) => parseInt(round)).sort((a, b) => Math.abs(a) - Math.abs(b));
@@ -11,6 +46,7 @@ function bracketAnalysis(bracket: BracketRounds): BracketData {
 
     const maxRound = Math.max(...absRounds);
     const roundOffset = Math.min(...absRounds);
+    // const roundOffset = maxRound - minRound;
 
     const maxLength = Math.max(...rounds.map((round) => bracket[round].length));
 
