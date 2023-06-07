9803ca2861d2177486864d03455e6ebbb1210013
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-06-07 22:13:41 +0530

    Simplified functionality
    - removed reduce sets function
    - Function to get all sets in between per round

--- src/MainScreen/Debug/BracketData.ts
+++ src/MainScreen/Debug/BracketData.ts
@@ -1,43 +1,40 @@
 import { BracketData, BracketRounds, FullBracket, FullBracketData, SetList } from "./types";
 
-
-export function reduceSets(sets: SetList) {
-    return sets.map(set => {
-        return {
-            id: set?.id,
-            round: set?.round,
-            slots: set?.slots ?? []
-        }
-    }) ?? [];
-}
-
 export function convertSets(sets: SetList) {
-    const setMap = reduceSets(sets);
+    const setMap = sets ?? []
 
-    const maxRound = Math.max(...setMap.map(set => set.round ?? -1));
-    const minRound = Math.min(...setMap.map(set => set.round ?? 1));
+    const maxRound = Math.max(...setMap.map(set => set?.round ?? -1));
+    const minRound = Math.min(...setMap.map(set => set?.round ?? 1));
 
-    const winnersBracket: BracketRounds = {}
-    for (let i=0;i<=maxRound;i++) {
-        const setsInRound = setMap.filter(set => set.round === i);
-        if (setsInRound.length > 0) {
-            winnersBracket[i] = setsInRound;
-        }
-    }
-
-    const losersBracket: BracketRounds = {};
-    for (let i=-1;i>minRound;i--) {
-        const setsInRound = setMap.filter(set => set.round === i);
-        if (setsInRound.length > 0) {
-            losersBracket[i] = setsInRound;
-        }
-    }
+    const winnersBracket = getSetsInRounds(0, maxRound, setMap);
+    const losersBracket = getSetsInRounds(-1, minRound, setMap);
 
     return {
         winners: winnersBracket,
         losers: losersBracket
     }
 
+}
+
+function getSetsInRounds(startRound: number, endRound: number, setList: SetList) {
+
+    const iterDirection = endRound - startRound >= 0 ? 1 : -1;
+    const startIter = Math.abs(startRound);
+    const endIter = Math.abs(endRound);
+
+    const bracketRounds: BracketRounds = {}
+
+    for (let i=startIter; i <= endIter; i++) {
+        const actualRound = iterDirection * i;
+        const setsInRound = setList.filter(set => set?.round === actualRound)
+            .sort((a, b) => a?.identifier?.localeCompare(b?.identifier ?? "XX") ?? -1);
+
+        if (setsInRound.length > 0) {
+            bracketRounds[actualRound] = setsInRound;
+        }
+    }
+
+    return bracketRounds;
 }
 
 function bracketAnalysis(bracket: BracketRounds): BracketData {
