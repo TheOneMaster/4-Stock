07ff97f73016b2ce3bf63bc4f6e59ccf881c7aa2
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-06-07 22:25:11 +0530

    use ForeignObject instead of pure svg
    - allows proper text wrapping and easier styling

--- src/MainScreen/Debug/MatchSVG.tsx
+++ src/MainScreen/Debug/MatchSVG.tsx
@@ -1,12 +1,11 @@
 import { useTheme } from "@react-navigation/native";
-import { G, Line, Rect, Text } from "react-native-svg";
-import { Match } from "./types";
+import { ForeignObject, G, Line, Rect, Text } from "react-native-svg";
+import { Match, MatchSlot } from "./types";
+import { StyleSheet, View } from "react-native";
+import { CustomText } from "../../Shared/Text";
 
 export const MATCH_WIDTH = 150;
 export const MATCH_HEIGHT = 50;
-const ROW_HEIGHT = MATCH_HEIGHT / 2;
-
-const SCORE_WIDTH = 25;
 
 
 
@@ -14,6 +13,41 @@ interface MatchResultProps {
     match: Match
     offsetX?: number
     offsetY?: number
+}
+
+interface ParticipantDetails {
+    name: string
+    sponsor: string | null
+}
+
+function getSlotScore(slot?: MatchSlot): string {
+
+    if (!slot) return "N/A"
+
+    const score = slot.standing?.stats?.score?.value;
+
+    if (score == undefined) return "N/A"
+    if (score === -1) return "DQ"
+
+    return score.toString()
+}
+
+function getSlotParticipants(slot?: MatchSlot): ParticipantDetails[] {
+    const defaultDetails: ParticipantDetails[] = [{name: "N/A", sponsor: null}]
+
+    if (!slot) return defaultDetails;
+
+    const participants = slot.standing?.entrant?.participants;
+    if (!participants) return [{name: slot.standing?.entrant?.name ?? "N/A", sponsor: null}];
+
+    const participantDetails: ParticipantDetails[] = participants.map(participant => {
+        return {
+            name: participant?.gamerTag ?? "N/A",
+            sponsor: participant?.prefix ?? null
+        }
+    })
+
+    return participantDetails
 }
 
 
@@ -24,68 +58,135 @@ export function MatchResult(props: MatchResultProps) {
         offsetY = 0,
         match
     } = props;
-    const {colors} = useTheme();
+    const { colors } = useTheme();
 
     const winnerSlot = match.slots?.find(slot => slot?.standing?.placement === 1);
     const loserSlot = match.slots?.find(slot => slot?.standing?.placement !== 1);
 
-    const winnerName = winnerSlot?.standing?.entrant?.name ?? "N/A";
-    const loserName = loserSlot?.standing?.entrant?.name ?? "N/A";
+    const winnerDetails = getSlotParticipants(winnerSlot);
+    const loserDetails = getSlotParticipants(loserSlot);
 
-    // TODO: If null or undefined, return score as NA; if score === -1, return DQ
-    const winnerScore = winnerSlot?.standing?.stats?.score?.value?.toString();
-    const loserScore = loserSlot?.standing?.stats?.score?.value?.toString();
+    // TODO: Change rendering based on whether there are multiple participants or a single one
+    const winnerName = winnerDetails.map(part => {
+        return part.sponsor ? `${part.sponsor}|${part.name}` : part.name;
+    }).join(" / ");
+    const loserName = loserDetails.map(part => part.name).join(" / ");
 
+    const winnerScore = getSlotScore(winnerSlot);
+    const loserScore = getSlotScore(loserSlot);
+
+    const winnerData: PlayerData[] = winnerDetails.map((player) => ({
+        name: player.name,
+        sponsor: player.sponsor,
+        score: winnerScore
+    }));
+
+    const loserData: PlayerData[] = loserDetails.map((player) => ({
+        name: player.name,
+        sponsor: player.sponsor,
+        score: loserScore
+    }))
 
     return (
         <>
+            <ForeignObject x={offsetX} y={offsetY} width={MATCH_WIDTH} height={MATCH_HEIGHT}>
+                <View style={[styles.container]}>
+                    <View style={styles.entrantRow}>
+                        <View style={styles.entrantName}>
+                            {winnerData[0].sponsor && <CustomText numberOfLines={1} style={{maxWidth: 30}}>{winnerData[0].sponsor}</CustomText>}
+                            {winnerData[0].sponsor && <CustomText> | </CustomText>}
+                            <CustomText numberOfLines={1}>{winnerData[0].name}</CustomText>
+                        </View>
+                        <View style={[styles.scoreBox, {backgroundColor: "green"}]}>
+                            <CustomText>{winnerData[0].score}</CustomText>
+                        </View>
+                    </View>
 
-            <G x={offsetX} y={offsetY}>
+                    <View style={{width: "100%", height: 1, backgroundColor: "grey"}} />
 
-                <EntrantRow x={0} y={0} entrantName={winnerName} score={winnerScore} scoreColor="green" />
-                <EntrantRow x={0} y={ROW_HEIGHT} entrantName={loserName} score={loserScore} scoreColor="red" />
-
-                <Rect x={0} y={0} width={MATCH_WIDTH} height={MATCH_HEIGHT} stroke={colors.border} strokeWidth={2} />
-                <Line x1={0} x2={MATCH_WIDTH} y={ROW_HEIGHT} stroke={colors.border} />
-                <Line x={MATCH_WIDTH - SCORE_WIDTH} y1={0} y2={MATCH_HEIGHT} stroke={colors.border} strokeWidth={2} />
-
-            </G>
-
+                    <View style={styles.entrantRow}>
+                        <View style={styles.entrantName}>
+                            {loserData[0].sponsor && <CustomText numberOfLines={1} style={{maxWidth: 40}}>{loserData[0].sponsor}</CustomText>}
+                            {loserData[0].sponsor && <CustomText> | </CustomText>}
+                            <CustomText numberOfLines={1} style={{flex: 1}}>{loserData[0].name}</CustomText>
+                        </View>
+                        <View style={[styles.scoreBox, {backgroundColor: "red"}]}>
+                            <CustomText>{loserData[0].score}</CustomText>
+                        </View>
+                    </View>
+                </View>
+            </ForeignObject>
         </>
     )
 }
 
-interface EntrantRowProps {
-    x: number
-    y: number
-    entrantName?: string | null
-    score?: string
-    scoreColor: string
+interface PlayerData {
+    name: string
+    sponsor?: string | null
+    score: string
 }
 
-function EntrantRow(props: EntrantRowProps) {
+interface TestMatchProps {
+    winner: PlayerData[]
+    loser: PlayerData[]
+}
 
-    const { colors } = useTheme();
-
-    const textOffsetY = props.y + (ROW_HEIGHT / 2) + 2;
-    const scoreTextOffset = props.x + (MATCH_WIDTH - SCORE_WIDTH) + (SCORE_WIDTH / 2);
-
-    const name = props.entrantName ?? "N/A"
-    const score = props.score?.toString() ?? "N/A"
+function TestMatch(props: TestMatchProps) {
 
     return (
-        <>
+        <View style={[styles.container]}>
+            <View style={styles.entrantRow}>
+                <View style={styles.entrantName}>
+                    {props.winner[0].sponsor && <CustomText numberOfLines={1} style={{maxWidth: 30}}>{props.winner[0].sponsor}</CustomText>}
+                    {props.winner[0].sponsor && <CustomText> | </CustomText>}
+                    <CustomText numberOfLines={1}>{props.winner[0].name}</CustomText>
+                </View>
+                <View style={[styles.scoreBox, {backgroundColor: "green"}]}>
+                    <CustomText>{props.winner[0].score}</CustomText>
+                </View>
+            </View>
 
-            <Text x={props.x + 5} y={textOffsetY} fontSize="16" fill={colors.text} alignmentBaseline="middle">{name}</Text>
-            <Rect
-                x={MATCH_WIDTH - SCORE_WIDTH}
-                y={props.y}
-                width={SCORE_WIDTH}
-                height={ROW_HEIGHT}
-                fill={props.scoreColor}
-            />
-            <Text x={scoreTextOffset} y={textOffsetY} fontSize="15" fill="white" alignmentBaseline="middle" textAnchor="middle">{score}</Text>
+            <View style={{width: "100%", height: 1, backgroundColor: "grey"}} />
 
-        </>
+            <View style={styles.entrantRow}>
+                <View style={styles.entrantName}>
+                    {props.loser[0].sponsor && <CustomText numberOfLines={1} style={{maxWidth: 40}}>{props.loser[0].sponsor}</CustomText>}
+                    {props.loser[0].sponsor && <CustomText> | </CustomText>}
+                    <CustomText numberOfLines={1} style={{flex: 1}}>{props.loser[0].name}</CustomText>
+                </View>
+                <View style={[styles.scoreBox, {backgroundColor: "red"}]}>
+                    <CustomText>{props.loser[0].score}</CustomText>
+                </View>
+            </View>
+        </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        width: 150,
+        height: 50,
+        borderWidth: 1,
+        borderColor: "grey",
+    },
+    entrantRow: {
+        flex: 1,
+        flexDirection: "row",
+    },
+    entrantName: {
+        flex: 1,
+        paddingLeft: 5,
+        overflow: "hidden",
+        flexDirection: "row"
+    },
+    scoreBox: {
+        paddingHorizontal: 5,
+        left: "auto"
+    },
+    seperator: {
+        width: "100",
+        height: 1,
+        backgroundColor: "grey"
+    }
+})
