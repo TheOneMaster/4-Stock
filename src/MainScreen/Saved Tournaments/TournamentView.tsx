a39f648a487ae72c4cacd448e607242929e69928
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-06-09 19:20:04 +0530

    Add selection functionality

--- src/MainScreen/Saved Tournaments/TournamentView.tsx
+++ src/MainScreen/Saved Tournaments/TournamentView.tsx
@@ -1,9 +1,11 @@
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useTheme } from "@react-navigation/native";
 import { StyleSheet, View } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
-import { runOnJS } from "react-native-reanimated";
+import Animated, { runOnJS, useAnimatedStyle } from "react-native-reanimated";
+import { AntDesign } from "@expo/vector-icons";
 
-import { PlaceholderImage, TransparentCard } from "../../Shared";
+import { useState } from "react";
+import { PlaceholderImage } from "../../Shared";
 import { TitleText } from "../../Shared/Text";
 import { SavedTournamentsCardNavigationProp } from "../../navTypes";
 import { TournamentData } from "./types";
@@ -13,15 +15,24 @@ import { TournamentData } from "./types";
 interface TournamentViewProps {
     data: TournamentData
     height: number
+    onPress?: () => void
+    longPress?: () => void
 }
 
-export function TournamentView({data, height}: TournamentViewProps) {
+export function TournamentView(props: TournamentViewProps) {
 
-    const navigation = useNavigation<SavedTournamentsCardNavigationProp>()
+    const [selected, setSelected] = useState(false);
 
-    const image = data.images?.find(image => image?.url)?.url;
-    const tournamentID = data.id;
+    const {colors} = useTheme();
+    const navigation = useNavigation<SavedTournamentsCardNavigationProp>();
 
+    const image = props.data.images?.find(image => image?.url)?.url;
+    const tournamentID = props.data.id;
+
+
+    const toggleSelected = () => {
+        setSelected((prevState) => !prevState)
+    }
 
     const clickGesture = Gesture.Tap().onStart(_ => {
         if (!tournamentID) return
@@ -29,23 +40,39 @@ export function TournamentView({data, height}: TournamentViewProps) {
     });
 
     const longPressGesture = Gesture.LongPress().onStart(() => {
-        console.log("Long Press Tournament: ", data.name);
+        console.log("Long Press Tournament: ", props.data.name);
+
+        runOnJS(toggleSelected)()
     })
 
     const allGestures = Gesture.Race(clickGesture, longPressGesture);
+
+    const animStyle = useAnimatedStyle(() => {
+        return {
+            borderColor: selected ? colors.primary : colors.border,
+            borderWidth: selected ? 3 : 1
+        }
+    })
 
 
 
     return (
         <GestureDetector gesture={allGestures}>
-            <TransparentCard style={styles.container}>
-                <View style={{height: height}}>
-                    <PlaceholderImage imageSrc={image} style={styles.image} resize="cover" />
+            <Animated.View style={[styles.container, animStyle]}>
+                <View style={{height: props.height}}>
+                    <PlaceholderImage imageSrc={image} placeholder="tournament" style={styles.image} resize="cover" />
                 </View>
                 <View>
-                    <TitleText style={styles.title} numberOfLines={2}>{data.name}</TitleText>
+                    <TitleText style={styles.title} numberOfLines={2}>{props.data.name}</TitleText>
                 </View>
-            </TransparentCard>
+
+                {selected
+                ? <View style={styles.selectionCircle}>
+                 <AntDesign name="check" color="black" />
+                </View>
+                : null}
+
+            </Animated.View>
         </GestureDetector>
     )
 
@@ -61,5 +88,18 @@ const styles = StyleSheet.create({
     },
     title: {
         textAlign: "center"
+    },
+    selectionCircle: {
+        position: "absolute",
+        right: 5,
+        top: 5,
+        backgroundColor: "white",
+        height: 20,
+        width: 20,
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 0.5,
+        borderColor: "black"
     }
 })
