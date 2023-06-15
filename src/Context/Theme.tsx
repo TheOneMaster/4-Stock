6eea6c35a962b060a0eaf38539316a3b3c90994e
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2023-06-15 23:12:58 +0530

    create Theme context

---
+++ src/Context/Theme.tsx
@@ -0,0 +1,34 @@
+import React, { createContext, useState } from "react";
+import { ColorValue } from "react-native";
+
+interface UIThemeOptions {
+    baseColor: ColorValue
+    secondaryColor: ColorValue
+    backgroundColor: ColorValue
+}
+
+interface ThemeContextProps {
+    theme: UIThemeOptions
+    changeTheme: (newTheme: UIThemeOptions) => void
+}
+
+interface ThemeProviderProps {
+    children: React.ReactNode
+}
+
+const ThemeContext = createContext<ThemeContextProps | null>(null);
+
+
+export function ThemeProvider(props: ThemeProviderProps) {
+    const [theme, setTheme] = useState<UIThemeOptions>({
+        baseColor: "#7DF9FF",
+        secondaryColor: "gold",
+        backgroundColor: "black"
+    });
+
+    return (
+        <ThemeContext.Provider value={{ theme, changeTheme: setTheme }}>
+            {props.children}
+        </ThemeContext.Provider>
+    )
+}
