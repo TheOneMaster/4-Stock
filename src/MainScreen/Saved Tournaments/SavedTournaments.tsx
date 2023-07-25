689504417f0c2268fe45222275283bbde106db22
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2023-07-25 18:27:16 +0530

    use context object to store saved tournaments

--- src/MainScreen/Saved Tournaments/SavedTournaments.tsx
+++ src/MainScreen/Saved Tournaments/SavedTournaments.tsx
@@ -1,20 +1,27 @@
 import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
-
+import { useSavedTournaments } from "../../Context/SavedTournaments";
 import { useSavedTournamentsQuery } from "../../gql/gql";
+
+// import { useSavedTournamentsQuery } from "../../gql/gql";
 
 import { truthyFilter } from "../../helper";
 import { SavedTournamentsScreenProps } from "../../navTypes";
 import { CenterMessage } from "../../Shared";
 import { IoniconsThemed } from "../../Shared/IconTheme";
+import { CustomText } from "../../Shared/Text";
 import { TournamentView } from "./TournamentView";
 
 export function SavedTournamentsPage({ navigation, route }: SavedTournamentsScreenProps) {
     // Tipped off 14, TBH 11, SSC '23, Shine '23
-    const saved = ["511041", "548572", "470565", "540064"];
+    // const saved = ["511041", "548572", "470565", "540064"];
 
+    // const { data, status } = useSavedTournamentsQuery({ ids: saved });
+    const { saved, updateSaved } = useSavedTournaments();
     const { data, status } = useSavedTournamentsQuery({ ids: saved });
+
+
 
     const [selectionMode, setSelectionMode] = useState(false);
     const [selected, setSelected] = useState<string[]>([]);
@@ -39,6 +46,9 @@ export function SavedTournamentsPage({ navigation, route }: SavedTournamentsScre
 
             if (prevSelected.includes(tournament)) {
                 const newState = prevSelected.filter(id => id !== tournament);
+
+                if (newState.length === 0) setSelectionMode(false);
+
                 return newState
             }
 
--- src/MainScreen/Saved Tournaments/tournamentsHook.tsx
+++
@@ -1,7 +0,0 @@
-import { useMMKVObject } from "react-native-mmkv";
-
-export function useSavedTournaments() {
-    const [saved, setSaved] = useMMKVObject<number[]>("saved.tournaments");
-    const returnArr = [saved ?? [], setSaved] as const;
-    return returnArr
-}
