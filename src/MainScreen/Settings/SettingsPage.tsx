af79ebb9135ba55557922f2113b0a9c9cdc70b51
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2023-07-27 09:22:13 +0530

    Only show debug option if built using debug mode

--- src/MainScreen/Settings/SettingsPage.tsx
+++ src/MainScreen/Settings/SettingsPage.tsx
@@ -1,12 +1,13 @@
 import { ScrollView, StyleSheet } from "react-native";
 
+import GAME_LIST from "./games.json";
 import { SettingsDropdown, SettingsSwitch, SettingsTextInput } from "./SettingsComponents";
 import SettingsGroup from "./SettingsGroup";
-import GAME_LIST from "./games.json";
 
 import { useSettings } from "../../Context";
 import { SettingsViewProps } from "../../navTypes";
 
+const IS_DEBUG = __DEV__;
 
 function SettingsPage({ navigation, route }: SettingsViewProps) {
 
@@ -18,7 +19,7 @@ function SettingsPage({ navigation, route }: SettingsViewProps) {
             <SettingsGroup title="General">
                 <SettingsTextInput title="API Key" icon="key-outline" value={settings.apiKey} updateValue={(v) => updateSetting("apiKey", v)} />
                 <SettingsDropdown title="Main Game" icon="game-controller-outline" updateValue={(v) => updateSetting("mainGame", v)} value={settings.mainGame} options={GAME_LIST} />
-                <SettingsSwitch title="Debug" icon="bug-outline" value={settings.debug} updateValue={(value) => updateSetting("debug", value)} />
+                {IS_DEBUG && <SettingsSwitch title="Debug" icon="bug-outline" value={settings.debug} updateValue={(value) => updateSetting("debug", value)} />}
             </SettingsGroup>
 
         </ScrollView>
