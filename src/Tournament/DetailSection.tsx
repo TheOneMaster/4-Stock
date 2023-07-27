339cc2227811f2b995728bcf5b41e407fb8a3c34
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2023-07-27 13:11:22 +0530

    Update Tournament Details to provide Icons
    - Also adds spacing between items for better legibility

--- src/Tournament/DetailSection.tsx
+++ src/Tournament/DetailSection.tsx
@@ -1,32 +1,74 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 import { DetailsSectionProps } from "./types";
-import { convertDateToString } from "../helper";
+import { convertDateToString, truthyFilter } from "../helper";
 import { CustomText } from "../Shared/Text";
+import { FontAwesomeThemed, IoniconsThemed, MaterialIconsThemed } from "../Shared/IconTheme";
 
 function DetailSection(props: DetailsSectionProps) {
     const startDate = convertDateToString(props.startAt);
     const lastDateRegister = convertDateToString(props.eventRegistrationClosesAt);
 
+    const location = props.addrState ? [props.city, props.addrState] : [props.city, props.countryCode];
+    const locationString = location.filter(truthyFilter).join(", ");
+    const numAttendees = props.numAttendees?.toString();
+
     return (
         <View style={styles.container}>
-            <View>
-                <CustomText>City: {props.city}</CustomText>
-                <CustomText>Starting At: {startDate}</CustomText>
-                <CustomText>Country: {props.countryCode}</CustomText>
-                <CustomText>Currency: {props.currency}</CustomText>
-                {props.eventRegistrationClosesAt && <CustomText>Last date for registration: {lastDateRegister}</CustomText>}
-                <CustomText>Number of attendees: {props.numAttendees}</CustomText>
-                <CustomText>Venue Address: {props.venueAddress}</CustomText>
-            </View>
+
+            <DetailRow title="Location" value={locationString} icon={<FontAwesomeThemed name="location-arrow" size={16} />} />
+            <DetailRow title="Starting At" value={startDate} icon={<FontAwesomeThemed name="calendar" size={16} />} />
+            <DetailRow title="Last date for registration" value={lastDateRegister} icon={<FontAwesomeThemed name="calendar" size={16} />} />
+            <DetailRow title="Number of attendees" value={numAttendees} icon={<IoniconsThemed name="people-outline" size={16} />} />
+            <DetailRow title="Venue Address" value={props.venueAddress} icon={<MaterialIconsThemed name="location-city" size={16} />} />
+
         </View>
     );
 
 };
 
+type DetailRowProps = {
+    title: string
+    value?: string | null
+    icon?: React.ReactNode
+}
+
+function DetailRow(props: DetailRowProps) {
+
+    if (!props.value) return null
+
+    return (
+        <View style={styles.row}>
+            <View style={styles.rowIcon}>
+                {props.icon ?? null}
+            </View>
+            <Text style={{ flexWrap: "wrap" }}>
+                <CustomText style={styles.rowType}>{props.title}: </CustomText>
+                <CustomText>{props.value}</CustomText>
+            </Text>
+        </View>
+    )
+}
+
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        flexWrap: "wrap",
+    },
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
+        // backgroundColor: "green",
+        // marginBottom: 5,
+        paddingVertical: 5
+    },
+    rowType: {
+        fontWeight: "bold"
+    },
+    rowIcon: {
+        marginRight: 5,
+        // width: 15,
+        // alignItems: "center"
     }
 })
 
