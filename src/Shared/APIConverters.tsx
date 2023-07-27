c53f0e5064e537526a0d4f67103b87466ec1d0dd
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2023-07-27 13:09:37 +0530

    Allow null/undefined values in the images array

--- src/Shared/APIConverters.tsx
+++ src/Shared/APIConverters.tsx
@@ -1,18 +1,25 @@
 import { Image } from "../gql/gql";
 
+type GenericImage<T> = (T | null)[];
+type MinimumImage = Pick<Image, "url">;
+type TypedImage = Pick<Image, "url" | "type">
+
 /**
  * Returns an image from an array of images based on the type of the image.
  * @param images Array of images
  * @param type The type of the image to be returned as a string. If any image is acceptable, pass "any" to the function
  * @returns Image with url
  */
-export function getImageByType<Type extends Pick<Image, "url">>(images: Type[], type: "any"): Type | Pick<Image, "url">;
-export function getImageByType<Type extends Pick<Image, "type" | "url">>(images: Type[], type: string | string[]): Type | Pick<Image, "url">;
-export function getImageByType<Type extends Pick<Image, "type" | "url">>(
-    images: Type[],
+export function getImageByType<T extends MinimumImage>(images: GenericImage<T>, type: "any"): T | Pick<Image, "url">;
+export function getImageByType<T extends TypedImage>(images: GenericImage<T>, type: string | string[]): T | Pick<Image, "url">;
+export function getImageByType<T extends TypedImage>(
+    images: GenericImage<T>,
     type: string | string[]
-): Type | Pick<Image, "url"> {
-    const finalImage = images.reduce<Type | null>((prev, cur) => {
+): T | Pick<Image, "url"> {
+    const finalImage = images.reduce<T | null>((prev, cur) => {
+
+        if (!cur) return prev
+
         let typeMatch: boolean
         if (typeof type === "string") {
             if (type === "any") {
