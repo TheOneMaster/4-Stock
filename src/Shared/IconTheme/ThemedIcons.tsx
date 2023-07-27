0fa218220f33b2c367bf240b3a8962b7d9b894d8
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2023-07-27 13:10:37 +0530

    Add themed MaterialIcons
    Moved types into the same file, removing abstraction

--- src/Shared/IconTheme/ThemedIcons.tsx
+++ src/Shared/IconTheme/ThemedIcons.tsx
@@ -1,7 +1,17 @@
 import { useTheme } from "@react-navigation/native";
-import { Ionicons, FontAwesome, AntDesign } from "@expo/vector-icons"
+import { Ionicons, FontAwesome, AntDesign, MaterialIcons } from "@expo/vector-icons"
+import { ComponentProps } from "react";
+import { Icon } from "@expo/vector-icons/build/createIconSet"
 
-import { AntDesignThemedProps, FontAwesomeThemedProps, IoniconsThemedProps } from "./types"
+type TextTypes = "primary" | "secondary";
+type Text = { text?: TextTypes }
+
+export type IoniconsThemedProps = ComponentProps<typeof Ionicons> & Text
+export type FontAwesomeThemedProps = ComponentProps<typeof FontAwesome> & Text
+export type AntDesignThemedProps = ComponentProps<typeof AntDesign> & Text
+export type MaterialIconsThemedProps = ComponentProps<typeof MaterialIcons> & Text
+
+
 
 export function IoniconsThemed(props: IoniconsThemedProps) {
     const { colors } = useTheme();
@@ -16,4 +26,9 @@ export function FontAwesomeThemed(props: FontAwesomeThemedProps) {
 export function AntDesignThemed(props: AntDesignThemedProps) {
     const { colors } = useTheme();
     return <AntDesign {...props} color={props.text === "secondary" ? colors.secondaryText : colors.text} />
+}
+
+export function MaterialIconsThemed(props: MaterialIconsThemedProps) {
+    const { colors } = useTheme();
+    return <MaterialIcons {...props} color={props.text === 'secondary' ? colors.secondaryText : colors.text} />
 }
